59f395dec30460a52a23eec7b3c973620984239d
gabrielavmxx <gabriela.montero@sptech.school>
2026-03-26 19:29:12 -0300

    projeto inicializado

---
+++ src/test/java/school/sptech/projeto_extensao/ProjetoExtensaoApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.projeto_extensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoExtensaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
