8fbe910b7c5ed65ce4be2c6a812f8537417e40e6
gabrielavmxx <gabriela.montero@sptech.school>
2026-04-02 18:49:48 -0300

    função buscar clientes, buscar por id e criação de clientes criada

--- src/main/java/school/sptech/projeto_extensao/controller/ClienteController.java
+++ src/main/java/school/sptech/projeto_extensao/controller/ClienteController.java
@@ -1,11 +1,11 @@
 package school.sptech.projeto_extensao.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.sptech.projeto_extensao.dto.ClienteMapper;
+import school.sptech.projeto_extensao.dto.ClienteRequestDto;
 import school.sptech.projeto_extensao.dto.ClienteResponseDto;
 import school.sptech.projeto_extensao.model.Cliente;
 import school.sptech.projeto_extensao.service.ClienteService;
@@ -27,6 +27,27 @@ public class ClienteController {
         if(clientes.isEmpty()){
             return ResponseEntity.status(404).build();
         }
-        List<ClienteResponseDto> responseDto = ClienteMapper.to
+        List<ClienteResponseDto> responseDto = ClienteMapper.toDto(clientes);
+        return ResponseEntity.status(200).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDto> buscarPorId(
+            @PathVariable Integer id
+    ){
+        Cliente cliente = clienteService.findById(id);
+        ClienteResponseDto responseDto = ClienteMapper.toDto(cliente);
+        return ResponseEntity.status(200).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteResponseDto> cadastrar(
+            @Valid @RequestBody ClienteRequestDto dto
+            ){
+        Cliente entidade = ClienteMapper.toEntity(dto);
+        Cliente clienteCadastrado = clienteService.cadastrar(entidade);
+
+        ClienteResponseDto resposta = ClienteMapper.toDto(clienteCadastrado);
+        return ResponseEntity.status(201).body(resposta);
     }
 }
