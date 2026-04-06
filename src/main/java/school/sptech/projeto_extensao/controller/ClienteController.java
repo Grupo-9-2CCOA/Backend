76f533d902cf41b082e9968cca7cb9a45fdbe786
gabrielavmxx <gabriela.montero@sptech.school>
2026-04-06 12:40:48 -0300

    mudanças do data.sql e buscar clientes com retorno

--- src/main/java/school/sptech/projeto_extensao/controller/ClienteController.java
+++ src/main/java/school/sptech/projeto_extensao/controller/ClienteController.java
@@ -20,6 +20,7 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
+    //Listar Clientes
     @GetMapping
     public ResponseEntity<List<ClienteResponseDto>> listar(){
         List<Cliente> clientes = clienteService.listar();
@@ -27,18 +28,20 @@ public class ClienteController {
             return ResponseEntity.status(404).build();
         }
         List<ClienteResponseDto> responseDto = ClienteMapper.toDto(clientes);
-        return ResponseEntity.status(200).build();
+        return ResponseEntity.status(200).body(responseDto);
     }
 
+    //Listar Cliente por id
     @GetMapping("/{id}")
     public ResponseEntity<ClienteResponseDto> buscarPorId(
             @PathVariable Integer id
     ){
         Cliente cliente = clienteService.findById(id);
         ClienteResponseDto responseDto = ClienteMapper.toDto(cliente);
-        return ResponseEntity.status(200).build();
+        return ResponseEntity.status(200).body(responseDto);
     }
 
+    //Criar Cliente
     @PostMapping
     public ResponseEntity<ClienteResponseDto> cadastrar(
             @Valid @RequestBody ClienteRequestDto dto
@@ -50,6 +53,7 @@ public class ClienteController {
         return ResponseEntity.status(201).body(resposta);
     }
 
+    //"Deletar" Cliente
     @PutMapping
     public ResponseEntity<Void> inativarCliente(
             @PathVariable Integer id
@@ -62,12 +66,13 @@ public class ClienteController {
         return ResponseEntity.status(200).build();
     }
 
-    @PutMapping
-    public ResponseEntity<ClienteResponseDto> atualizar(
-            @RequestBody @Valid ClienteRequestDto cliente,
-            @PathVariable Integer id
-    ){
-        ClienteMapper.toDto(clienteService.atualizar(id, ClienteMapper.toEntity(cliente)));
-        return ResponseEntity.status(200).build();
-    }
+//    //Atualizar Cliente
+//    @PutMapping
+//    public ResponseEntity<ClienteResponseDto> atualizar(
+//            @RequestBody @Valid ClienteRequestDto cliente,
+//            @PathVariable Integer id
+//    ){
+//        ClienteMapper.toDto(clienteService.atualizar(id, ClienteMapper.toEntity(cliente)));
+//        return ResponseEntity.status(200).build();
+//    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,18 @@
-CREATE TABLE admin (
+CREATE TABLE IF NOT EXISTS admin (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(30) NOT NULL,
     senha VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE cliente (
+CREATE TABLE IF NOT EXISTS cliente (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    telefone VARCHAR(11)
+    telefone VARCHAR(11),
+    ativo BOOLEAN
 );
 
-CREATE TABLE endereco (
+CREATE TABLE IF NOT EXISTS endereco (
     id INT AUTO_INCREMENT PRIMARY KEY,
     logradouro VARCHAR(30) NOT NULL,
     numero VARCHAR(30) NOT NULL,
@@ -21,43 +22,43 @@ CREATE TABLE endereco (
     FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 );
 
-CREATE TABLE entrega(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS entrega(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     estado VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE pagamento(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS pagamento(
+    id INT AUTO_INCREMENT PRIMARY KEY,
     estado VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE pedido (
+CREATE TABLE IF NOT EXISTS pedido (
     id INT AUTO_INCREMENT,
     id_cliente INT,
     id_endereco INT,
     produto VARCHAR(100) NOT NULL,
     descricao VARCHAR(200),
     valor DECIMAL(10,2) NOT NULL,
-	status_entrega INT NOT NULL,
+    status_entrega INT NOT NULL,
     status_pagamento INT NOT NULL,
     data_pedido DATE,
     PRIMARY KEY (id, id_cliente),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id),
     FOREIGN KEY (id_endereco) REFERENCES endereco(id),
-	FOREIGN KEY (status_entrega) REFERENCES entrega(id),
+    FOREIGN KEY (status_entrega) REFERENCES entrega(id),
     FOREIGN KEY (status_pagamento) REFERENCES pagamento(id)
 );
 
-CREATE TABLE historico_pedido (
-	id INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS historico_pedido (
+    id INT AUTO_INCREMENT,
     id_pedido INT,
     id_cliente INT,
-	status_entrega INT NOT NULL,
+    status_entrega INT NOT NULL,
     status_pagamento INT NOT NULL,
-	data_modificacao DATE,
-	PRIMARY KEY (id, id_pedido, id_cliente),
+    data_modificacao DATE,
+    PRIMARY KEY (id, id_pedido, id_cliente),
     FOREIGN KEY (id_pedido) REFERENCES pedido(id),
-	FOREIGN KEY (id_cliente) REFERENCES pedido(id_cliente),
-	FOREIGN KEY (status_entrega) REFERENCES entrega(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id),
+    FOREIGN KEY (status_entrega) REFERENCES entrega(id),
     FOREIGN KEY (status_pagamento) REFERENCES pagamento(id)
-);
+    );
