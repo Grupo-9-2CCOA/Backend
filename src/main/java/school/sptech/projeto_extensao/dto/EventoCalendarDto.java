6db0c84cd422131585375d529359d6c06cf4f21d
guilhermesdias45 <guilherme.sdias@sptech.school>
2026-05-31 23:24:37 -0300

    feat: configuração do Google Calendar API

---
+++ src/main/java/school/sptech/projeto_extensao/dto/EventoCalendarDto.java
@@ -0,0 +1,21 @@
+package school.sptech.projeto_extensao.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class EventoCalendarDto {
+
+    @NotBlank(message = "O título do evento é obrigatório")
+    private String titulo;
+
+    private String descricao;
+
+    @NotNull(message = "A data de início é obrigatória")
+    private LocalDateTime dataInicio;
+
+    @NotNull(message = "A data de término é obrigatória")
+    private LocalDateTime dataFim;
+}
